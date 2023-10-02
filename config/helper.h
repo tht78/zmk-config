800d4f5dfe44c57c945715651e4c931e05420d85
tht78 <thohtor@gmail.com>
2023-10-02 02:56:33 +0200

    bugs

--- config/helper.h
+++ config/helper.h
@@ -153,8 +153,8 @@
     UC_MACRO(name ## _lower, &kp L0 &kp L1 &kp L2 &kp L3) \
     UC_MODMORPH(name, &name ## _lower, &none)
 
-#define ZMK_UNICODE_PAIR(name, L0, L1, L2, L3, U0, U1, U2, U3) \
-    UC_MACRO(name ## _lower, &kp L0 &kp L1 &kp L2 &kp L3) \
-    UC_MACRO(name ## _upper, &kp U0 &kp U1 &kp U2 &kp U3) \
-    UC_MODMORPH(name, &name ## _lower, &name ## _upper)
+//#define ZMK_UNICODE_PAIR(name, L0, L1, L2, L3, U0, U1, U2, U3) \
+//    UC_MACRO(name ## _lower, &kp L0 &kp L1 &kp L2 &kp L3) \
+//    UC_MACRO(name ## _upper, &kp U0 &kp U1 &kp U2 &kp U3) \
+//    UC_MODMORPH(name, &name ## _lower, &name ## _upper)
 
