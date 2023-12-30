bce5219627e033ec0c2c4f7ac786c13cf1fa74a5
tht78 <thohtor@gmail.com>
2023-12-30 16:03:31 +0100

    update

--- config/helper.h
+++ config/helper.h
@@ -27,7 +27,7 @@
     / { \
         behaviors { \
             name: name { \
-                /* label = ZMK_HELPER_STRINGIFY(ZB_ ## name); \ */
+                /* label = ZMK_HELPER_STRINGIFY(ZB_ ## name); */\
                 ZMK_BEHAVIOR_CORE_ ## type; \
                 __VA_ARGS__ \
             }; \
@@ -43,7 +43,7 @@
         keymap { \
             compatible = "zmk,keymap"; \
             layer_ ## name { \
-                /* label = ZMK_HELPER_STRINGIFY(name); \ */
+                /* label = ZMK_HELPER_STRINGIFY(name);*/ \
                 bindings = <layout>; \
             }; \
         }; \
@@ -53,7 +53,7 @@
         keymap { \
             compatible = "zmk,keymap"; \
             layer_ ## name { \
-                /* label = ZMK_HELPER_STRINGIFY(name); \ */
+                /* label = ZMK_HELPER_STRINGIFY(name); */\
                 bindings = <layout>; \
                 sensor-bindings = <sensors>; \
             }; \
@@ -127,7 +127,7 @@
         macros { \
             name: name { \
                 compatible = "zmk,behavior-macro"; \
-                /* label = ZMK_HELPER_STRINGIFY(UC_MACRO_ ## name); \ */
+                /* label = ZMK_HELPER_STRINGIFY(UC_MACRO_ ## name); */\
                 wait-ms = <0>; \
                 tap-ms = <0>; \
                 #binding-cells = <0>; \
@@ -141,7 +141,7 @@
         behaviors { \
             name: name { \
                 compatible = "zmk,behavior-mod-morph"; \
-                /* label = ZMK_HELPER_STRINGIFY(UC_MORPH_ ## name); \ */
+                /* label = ZMK_HELPER_STRINGIFY(UC_MORPH_ ## name); */\
                 #binding-cells = <0>; \
                 bindings = <uc_binding>, <shifted_uc_binding>; \
                 mods = <(MOD_LSFT|MOD_RSFT)>; \
